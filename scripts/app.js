5fcfc54f2fa063d6a7c8d23097a1070950dbefdf
ZenixR1 <171113912+ZenixR1@users.noreply.github.com>
2025-04-17 22:28:36 -0400

    Added styling to the body, header, contentContainer, inputContainer, and the input field. Provided a placeholder. Removed the add button and switched the functionality to include pressing Enter to submit. Added refresh clearing the input field as well as when you submit a task.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
 console.log('Welcome to the ToDo List!');
 
-//DOM for input
+//DOM for body
+const body = document.querySelector('body');
+//DOM for input and make sure it is empty
 const createTask = document.querySelector('#createTask');
+createTask.value = '';
 //DOM for add button
 const addBtn = document.querySelector('#addBtn');
 //DOM for il container
@@ -10,77 +13,97 @@ const liContainer = document.querySelector('.li-container');
 const header = document.querySelector('.headerDiv');
 //DOM for content-container
 const contentContainer = document.querySelector('.content-container')
+//DOM for input-container
+const inputContainer = document.querySelector('.input-container');
 
-//Events listener for add button
-addBtn.addEventListener("click",addToDo)
+
+
+body.style.backgroundColor = '#333333';//styling for the body
+
+header.style.backgroundColor = '#1a1a1a';//styling for header
+
+contentContainer.style.backgroundColor = '#1a1a1a'//styling for the contentContainer
+
+inputContainer.style.padding = '5px';
+inputContainer.style.border = '2px solid black';
+inputContainer.style.borderRadius = '8px';
+
+//Adding placeholder text to input field as well as padding.
+createTask.placeholder = 'Select Enter to add Task!';
+createTask.style.padding = '8px';
+
+createTask.addEventListener("keydown",function(event){
+    if (event.key === "Enter"){
+        addToDo();
+        console.log('test');
+    }
+});//Events listener for add button
 
 function addToDo(){
     const task = createTask.value; //grabs the value from the input
     const todoContainer = document.createElement('div');
-/*
-//setting the todo-container to be flex (column)
-    todoContainer.style.display = 'flex';
-    todoContainer.style.flexDirection = 'column';
-    todoContainer.style.alignItems = 'center'; 
-    */
 
-//setting the li-container to be flex column
-    liContainer.style.display = 'flex';
-    liContainer.style.flexDirection = 'column';
+    if (task != ''){ //This if statement prevents an empty submission from being passed.
+        //setting the li-container to be flex column
+            liContainer.style.display = 'flex';
+            liContainer.style.flexDirection = 'column';
 
-//setting the todo-container to be flex row
-    todoContainer.style.display = 'flex';
-    todoContainer.style.flexDirection = 'row';
-// create the checkbox div and style it
-    const checkboxContainer = document.createElement('div');
-    checkboxContainer.style.backgroundColor = 'whitesmoke';
-    checkboxContainer.style.border = '2px solid black';
-    checkboxContainer.style.borderRadius = '8px';
-    checkboxContainer.style.padding = '5px';
-    checkboxContainer.innerHTML = 
-    `
-    <input type="checkbox" id="taskCheck"/>
-    `;
-    checkboxContainer.style.marginRight = '5px';
+        //setting the todo-container to be flex row
+            todoContainer.style.display = 'flex';
+            todoContainer.style.flexDirection = 'row';
+        // create the checkbox div and style it
+            const checkboxContainer = document.createElement('div');
+            checkboxContainer.style.backgroundColor = '#333333';
+            checkboxContainer.style.border = '2px solid black';
+            checkboxContainer.style.borderRadius = '8px';
+            checkboxContainer.style.padding = '5px';
+            checkboxContainer.innerHTML = 
+            `
+            <input type="checkbox" id="taskCheck"/>
+            `;
+            checkboxContainer.style.marginRight = '5px';
 
-    const createLi = document.createElement('div'); 
-    createLi.classList.add('.createdTask');
-    createLi.innerHTML = 
-    `
-        <li>${task}</li>
-    `;
-    //add styling
-    createLi.style.backgroundColor = 'whitesmoke';
-    createLi.style.listStyle = 'none';
-    createLi.style.border = '2px solid black';
-    createLi.style.borderRadius = '8px';
-    createLi.style.display = 'flex';
-    createLi.style.flexDirection = 'row';
-    createLi.style.textAlign = 'center';
+            const createLi = document.createElement('div'); //This creates the div that houses the task.
+            createLi.classList.add('.createdTask');  // --- lines 47-58 are additions to the createLi div.
+            createLi.innerHTML = 
+            `
+                <li>${task}</li>
+            `;
+            createLi.style.backgroundColor = '#333333';
+            createLi.style.listStyle = 'none';
+            createLi.style.border = '2px solid black';
+            createLi.style.borderRadius = '8px';
+            createLi.style.display = 'flex';
+            createLi.style.flexDirection = 'row';
+            createLi.style.textAlign = 'center';
+            createLi.style.padding = '5px';
 
-    todoContainer.append(checkboxContainer);
-    todoContainer.append(createLi);
+            todoContainer.append(checkboxContainer); //These two combine the checkbox div and the task li divs. 
+            todoContainer.append(createLi);
 
-    liContainer.append(todoContainer);
-    
-    //DOM for Done button
-    // ---
-    const completeTask =  checkboxContainer.querySelector("#taskCheck");
-    completeTask.addEventListener('change', toggleDone);
+            liContainer.append(todoContainer);//This adds the combined divs to the UL "li-container"
+            
+            createTask.value = ''; //Clear the text after submission.
 
-    function toggleDone(){
-        const text = createLi.querySelector("li");
-        
-        if(this.checked){
-            text.style.backgroundColor = 'green';
-        }
+            //DOM for Done button
+            // ---
+            const completeTask =  checkboxContainer.querySelector("#taskCheck");
+            completeTask.addEventListener('change', toggleDone);
 
-        if (text.style.textDecoration === 'line-through'){
-            text.style.textDecoration = "none";
-        } else {
-            text.style.textDecoration = 'line-through'
-            console.log('Task Done!');
-        }
+            function toggleDone(){
+                const text = createLi.querySelector("li");
+                
+                if(this.checked){
+                    text.style.backgroundColor = 'green';
+                }
+
+                if (text.style.textDecoration === 'line-through'){
+                    text.style.textDecoration = "none";
+                } else {
+                    text.style.textDecoration = 'line-through'
+                    console.log('Task Done!');
+                }
+            }
     }
 
 }
